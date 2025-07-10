340b3623bfd440eddfafce6c0fdd971e8b6caea0
wanter9091 <dhqudwo2976@naver.com>
2025-07-10 16:56:24 +0900

    테스트

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React test
+          Learn React test test
         </a>
       </header>
     </div>
